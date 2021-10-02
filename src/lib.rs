c7c901adecc8bab5f64c9b48a40f310b1dda6a20
Liam Fissell <{ID}+{username}@users.noreply.github.com>
2021-10-02 00:25:51 -0500

    hopefully working stereo and such

--- src/lib.rs
+++ src/lib.rs
@@ -43,13 +43,20 @@ impl Default for ReverbModel {
 }
 
 struct Reverb {
-    allpass_one: Allpass,
-    allpass_two: Allpass,
-    allpass_three: Allpass,
-    delay_one: DegradedDelay,
-    delay_two: DegradedDelay,
-    delay_three: DegradedDelay,
-    delay_four: DegradedDelay,
+    allpass_one_l: Allpass,
+    allpass_two_l: Allpass,
+    allpass_three_l: Allpass,
+    delay_one_l: DegradedDelay,
+    delay_two_l: DegradedDelay,
+    delay_three_l: DegradedDelay,
+    delay_four_l: DegradedDelay,
+    allpass_one_r: Allpass,
+    allpass_two_r: Allpass,
+    allpass_three_r: Allpass,
+    delay_one_r: DegradedDelay,
+    delay_two_r: DegradedDelay,
+    delay_three_r: DegradedDelay,
+    delay_four_r: DegradedDelay,
 }
 
 impl Plugin for Reverb {
@@ -65,13 +72,20 @@ impl Plugin for Reverb {
     #[inline]
     fn new(sample_rate: f32, _model: &ReverbModel) -> Self {
         Self {
-            allpass_one: Allpass::new(sample_rate, 4410, 0.7),
-            allpass_two: Allpass::new(sample_rate, 2999, -0.7),
-            allpass_three: Allpass::new(sample_rate, 2646, 0.7),
-            delay_one: DegradedDelay::new(sample_rate, 1323, 0.7),
-            delay_two: DegradedDelay::new(sample_rate, 1499, 0.7),
-            delay_three: DegradedDelay::new(sample_rate, 1676, 0.7),
-            delay_four: DegradedDelay::new(sample_rate, 1852, 0.7),
+            allpass_one_l: Allpass::new(sample_rate, 4410, 0.7),
+            allpass_two_l: Allpass::new(sample_rate, 2999, -0.7),
+            allpass_three_l: Allpass::new(sample_rate, 2646, 0.7),
+            delay_one_l: DegradedDelay::new(sample_rate, 1323, 0.7),
+            delay_two_l: DegradedDelay::new(sample_rate, 1499, 0.7),
+            delay_three_l: DegradedDelay::new(sample_rate, 1676, 0.7),
+            delay_four_l: DegradedDelay::new(sample_rate, 1852, 0.7),
+            allpass_one_r: Allpass::new(sample_rate, 4410, 0.7),
+            allpass_two_r: Allpass::new(sample_rate, 2999, -0.7),
+            allpass_three_r: Allpass::new(sample_rate, 2646, 0.7),
+            delay_one_r: DegradedDelay::new(sample_rate, 1323, 0.7),
+            delay_two_r: DegradedDelay::new(sample_rate, 1499, 0.7),
+            delay_three_r: DegradedDelay::new(sample_rate, 1676, 0.7),
+            delay_four_r: DegradedDelay::new(sample_rate, 1852, 0.7),
         }
     }
 
@@ -81,41 +95,75 @@ impl Plugin for Reverb {
         let output = &mut ctx.outputs[0].buffers;
 
         for i in 0..ctx.nframes {
-            self.allpass_one.set_g(model.g[i]);
-            self.allpass_two.set_g(-model.g[i]);
-            self.allpass_three.set_g(model.g[i]);
+            self.allpass_one_l.set_g(model.g[i]);
+            self.allpass_two_l.set_g(-model.g[i]);
+            self.allpass_three_l.set_g(model.g[i]);
 
-            self.delay_one.set_feedback(model.g[i]);
-            self.delay_two.set_feedback(model.g[i]);
-            self.delay_three.set_feedback(model.g[i]);
-            self.delay_four.set_feedback(model.g[i]);
+            self.delay_one_l.set_feedback(model.g[i]);
+            self.delay_two_l.set_feedback(model.g[i]);
+            self.delay_three_l.set_feedback(model.g[i]);
+            self.delay_four_l.set_feedback(model.g[i]);
 
-            self.delay_one.set_feedback(1.0 - model.damping[i]);
-            self.delay_two.set_feedback(1.0 - model.damping[i]);
-            self.delay_three.set_feedback(1.0 - model.damping[i]);
-            self.delay_four.set_feedback(1.0 - model.damping[i]);
+            self.delay_one_l.set_feedback(1.0 - model.damping[i]);
+            self.delay_two_l.set_feedback(1.0 - model.damping[i]);
+            self.delay_three_l.set_feedback(1.0 - model.damping[i]);
+            self.delay_four_l.set_feedback(1.0 - model.damping[i]);
 
-            self.delay_one.set_amt(model.degrade_intensity[i] as i32);
-            self.delay_two.set_amt(model.degrade_intensity[i] as i32);
-            self.delay_three.set_amt(model.degrade_intensity[i] as i32);
-            self.delay_four.set_amt(model.degrade_intensity[i] as i32);
+            self.delay_one_l.set_amt(model.degrade_intensity[i] as i32);
+            self.delay_two_l.set_amt(model.degrade_intensity[i] as i32);
+            self.delay_three_l.set_amt(model.degrade_intensity[i] as i32);
+            self.delay_four_l.set_amt(model.degrade_intensity[i] as i32);
 
-            self.delay_one.set_ratio(model.degrade_speed[i]);
-            self.delay_two.set_ratio(model.degrade_speed[i]);
-            self.delay_three.set_ratio(model.degrade_speed[i]);
-            self.delay_four.set_ratio(model.degrade_speed[i]);
+            self.delay_one_l.set_ratio(model.degrade_speed[i]);
+            self.delay_two_l.set_ratio(model.degrade_speed[i]);
+            self.delay_three_l.set_ratio(model.degrade_speed[i]);
+            self.delay_four_l.set_ratio(model.degrade_speed[i]);
 
-            let delays_summed = (self.delay_one.process_sample(input[0][i])
-                + self.delay_two.process_sample(input[0][i])
-                + self.delay_three.process_sample(input[0][i])
-                + self.delay_four.process_sample(input[0][i]))
+            self.allpass_one_r.set_g(model.g[i]);
+            self.allpass_two_r.set_g(-model.g[i]);
+            self.allpass_three_r.set_g(model.g[i]);
+
+            self.delay_one_r.set_feedback(model.g[i]);
+            self.delay_two_r.set_feedback(model.g[i]);
+            self.delay_three_r.set_feedback(model.g[i]);
+            self.delay_four_r.set_feedback(model.g[i]);
+
+            self.delay_one_r.set_feedback(1.0 - model.damping[i]);
+            self.delay_two_r.set_feedback(1.0 - model.damping[i]);
+            self.delay_three_r.set_feedback(1.0 - model.damping[i]);
+            self.delay_four_r.set_feedback(1.0 - model.damping[i]);
+
+            self.delay_one_r.set_amt(model.degrade_intensity[i] as i32);
+            self.delay_two_r.set_amt(model.degrade_intensity[i] as i32);
+            self.delay_three_r.set_amt(model.degrade_intensity[i] as i32);
+            self.delay_four_r.set_amt(model.degrade_intensity[i] as i32);
+
+            self.delay_one_r.set_ratio(model.degrade_speed[i]);
+            self.delay_two_r.set_ratio(model.degrade_speed[i]);
+            self.delay_three_r.set_ratio(model.degrade_speed[i]);
+            self.delay_four_r.set_ratio(model.degrade_speed[i]);
+
+            let delays_summed_l = (self.delay_one_l.process_sample(input[0][i])
+                + self.delay_two_l.process_sample(input[0][i])
+                + self.delay_three_l.process_sample(input[0][i])
+                + self.delay_four_l.process_sample(input[0][i]))
                 / 2.0;
-            output[0][i] = ((self.allpass_three.process_sample(
-                self.allpass_two
-                    .process_sample(self.allpass_one.process_sample(delays_summed)),
+            let delays_summed_r = (self.delay_one_r.process_sample(input[0][i])
+                + self.delay_two_r.process_sample(input[0][i])
+                + self.delay_three_r.process_sample(input[0][i])
+                + self.delay_four_r.process_sample(input[0][i]))
+                / 2.0;
+            output[0][i] = ((self.allpass_three_l.process_sample(
+                self.allpass_two_l
+                    .process_sample(self.allpass_one_l.process_sample(delays_summed_l)),
             )) * model.g[i])
                 + input[0][i];
-            output[1][i] = input[1][i];
+            
+                output[1][i] = ((self.allpass_three_r.process_sample(
+                    self.allpass_two_r
+                        .process_sample(self.allpass_one_r.process_sample(delays_summed_r)),
+                )) * model.g[i])
+                    + input[1][i];
         }
     }
 }
