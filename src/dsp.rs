1915e4b02a75ec29730b8323a39632d6bb2f591c
Liam Fissell <{ID}+{username}@users.noreply.github.com>
2021-10-01 22:21:06 -0500

    new version with comb filtering, dampening, and downsampling

--- src/dsp.rs
+++ src/dsp.rs
@@ -1,3 +1,32 @@
+pub struct Downsampler {
+    amt: i32,
+    count: i32,
+    stored_sample: f32,
+}
+
+impl Downsampler {
+    pub fn new(amt: i32) -> Self {
+        Self {
+            amt,
+            count: 0,
+            stored_sample: 0.0,
+        }
+    }
+
+    pub fn process_sample(&mut self, input: f32, ratio: f32) -> f32 {
+        self.count = (self.count + 2) % (self.amt + 1);
+        if self.count == 0 {
+            self.stored_sample = input;
+        }
+
+        (ratio * self.stored_sample) + ((1.0 - ratio) * input)
+    }
+
+    pub fn set_amt(&mut self, amt: i32) {
+        self.amt = amt;
+    }
+}
+
 pub struct Delay {
     buffer: Vec<f32>,
     input_i: usize,
@@ -23,7 +52,7 @@ impl Delay {
     pub fn process_sample(&mut self, input: f32) -> f32 {
         self.buffer[self.input_i] = input;
         self.former_output = self.buffer[self.output_i];
-        
+
         self.input_i = (self.input_i + 1).rem_euclid(self.buffer.len());
         self.output_i = (self.output_i + 1).rem_euclid(self.buffer.len());
 
@@ -35,7 +64,7 @@ pub struct DelayWithFeedback {
     delay: Delay,
     feedback_delay: Delay,
     feedback: f32,
-    former: f32, //former output of main delay
+    former: f32,          //former output of main delay
     former_feedback: f32, //variable name makes it not sound like this, but it's the former output of the feedback delay
 }
 
@@ -57,10 +86,68 @@ impl DelayWithFeedback {
         self.delay.set_time(time);
         self.feedback_delay.set_time(time);
     }
-    
+
     pub fn process_sample(&mut self, input: f32) -> f32 {
-        self.former = self.delay.process_sample(input + (self.feedback * self.former_feedback));
+        self.former = self
+            .delay
+            .process_sample(input + (self.feedback * self.former_feedback));
         self.former_feedback = self.feedback_delay.process_sample(self.former);
+
+        self.former
+    }
+}
+
+pub struct DegradedDelay {
+    delay: Delay,
+    feedback_delay: Delay,
+    feedback: f32,
+    former: f32,          //former output of main delay
+    former_feedback: f32, //variable name makes it not sound like this, but it's the former output of the feedback delay
+    a: f32,
+    ratio: f32,
+    downsampler: Downsampler,
+}
+
+impl DegradedDelay {
+    pub fn new(sample_rate: f32, time: i32, feedback: f32) -> Self {
+        Self {
+            delay: Delay::new(sample_rate, time),
+            feedback_delay: Delay::new(sample_rate, time),
+            feedback,
+            former: 0.0,
+            former_feedback: 0.0,
+            a: 1.0,
+            ratio: 0.0,
+            downsampler: Downsampler::new(0),
+        }
+    }
+
+    pub fn set_feedback(&mut self, feedback: f32) {
+        self.feedback = feedback;
+    }
+    pub fn set_time(&mut self, time: i32) {
+        self.delay.set_time(time);
+        self.feedback_delay.set_time(time);
+    }
+    pub fn set_a(&mut self, a: f32) {
+        self.a = a;
+    }
+    pub fn set_ratio(&mut self, ratio: f32) {
+        self.ratio = ratio;
+    }
+    pub fn set_amt(&mut self, amt: i32) {
+        self.downsampler.set_amt(amt);
+    }
+
+    pub fn process_sample(&mut self, input: f32) -> f32 {
+        self.former = self
+            .delay
+            .process_sample(input + (self.feedback * self.former_feedback));
+        self.former_feedback = self.downsampler.process_sample(
+            ((1.0 - self.a) * self.former_feedback)
+                + (self.feedback_delay.process_sample(self.former) * self.a),
+            self.ratio,
+        );
 
         self.former
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,12 @@
-
 #![allow(incomplete_features)]
 #![feature(generic_associated_types)]
 
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
-use baseplug::{
-    ProcessContext,
-    Plugin,
-};
+use baseplug::{Plugin, ProcessContext};
 
 mod dsp;
-use dsp::Allpass;
+use dsp::{Allpass, DegradedDelay};
 
 baseplug::model! {
     #[derive(Debug, Serialize, Deserialize)]
@@ -18,6 +14,15 @@ baseplug::model! {
         #[model(min = 0.0, max = 1.0)]
         #[parameter(name = "g")]
         g: f32,
+        #[model(min = 0.0, max = 1.0)]
+        #[parameter(name = "damping")]
+        damping: f32,
+        #[model(min = 0.0, max = 29.0)]
+        #[parameter(name = "degrade_intensity")]
+        degrade_intensity: f32,
+        #[model(min = 0.0, max = 1.0)]
+        #[parameter(name = "degrade_speed")]
+        degrade_speed: f32,
     }
 }
 
@@ -25,6 +30,9 @@ impl Default for ReverbModel {
     fn default() -> Self {
         Self {
             g: 0.7,
+            damping: 0.0,
+            degrade_intensity: 0.0,
+            degrade_speed: 0.0,
         }
     }
 }
@@ -33,8 +41,10 @@ struct Reverb {
     allpass_one: Allpass,
     allpass_two: Allpass,
     allpass_three: Allpass,
-    allpass_four: Allpass,
-    allpass_five: Allpass,
+    delay_one: DegradedDelay,
+    delay_two: DegradedDelay,
+    delay_three: DegradedDelay,
+    delay_four: DegradedDelay,
 }
 
 impl Plugin for Reverb {
@@ -53,8 +63,10 @@ impl Plugin for Reverb {
             allpass_one: Allpass::new(sample_rate, 4410, 0.7),
             allpass_two: Allpass::new(sample_rate, 2999, -0.7),
             allpass_three: Allpass::new(sample_rate, 2646, 0.7),
-            allpass_four: Allpass::new(sample_rate, 869, 0.7),
-            allpass_five: Allpass::new(sample_rate, 258, 0.7),
+            delay_one: DegradedDelay::new(sample_rate, 1323, 0.7),
+            delay_two: DegradedDelay::new(sample_rate, 1499, 0.7),
+            delay_three: DegradedDelay::new(sample_rate, 1676, 0.7),
+            delay_four: DegradedDelay::new(sample_rate, 1852, 0.7),
         }
     }
 
@@ -62,14 +74,45 @@ impl Plugin for Reverb {
     fn process(&mut self, model: &ReverbModelProcess, ctx: &mut ProcessContext<Self>) {
         let input = &ctx.inputs[0].buffers;
         let output = &mut ctx.outputs[0].buffers;
-	
-	    for i in 0..ctx.nframes {
-            //self.allpass.set_g(model.g[i]);
-            
-            output[0][i] = self.allpass_five.process_sample(self.allpass_four.process_sample(self.allpass_three.process_sample(self.allpass_two.process_sample(self.allpass_one.process_sample(input[0][i])))));
+
+        for i in 0..ctx.nframes {
+            self.allpass_one.set_g(model.g[i]);
+            self.allpass_two.set_g(-model.g[i]);
+            self.allpass_three.set_g(model.g[i]);
+
+            self.delay_one.set_feedback(model.g[i]);
+            self.delay_two.set_feedback(model.g[i]);
+            self.delay_three.set_feedback(model.g[i]);
+            self.delay_four.set_feedback(model.g[i]);
+
+            self.delay_one.set_feedback(1.0 - model.damping[i]);
+            self.delay_two.set_feedback(1.0 - model.damping[i]);
+            self.delay_three.set_feedback(1.0 - model.damping[i]);
+            self.delay_four.set_feedback(1.0 - model.damping[i]);
+
+            self.delay_one.set_amt(model.degrade_intensity[i] as i32);
+            self.delay_two.set_amt(model.degrade_intensity[i] as i32);
+            self.delay_three.set_amt(model.degrade_intensity[i] as i32);
+            self.delay_four.set_amt(model.degrade_intensity[i] as i32);
+
+            self.delay_one.set_ratio(model.degrade_speed[i]);
+            self.delay_two.set_ratio(model.degrade_speed[i]);
+            self.delay_three.set_ratio(model.degrade_speed[i]);
+            self.delay_four.set_ratio(model.degrade_speed[i]);
+
+            let delays_summed = (self.delay_one.process_sample(input[0][i])
+                + self.delay_two.process_sample(input[0][i])
+                + self.delay_three.process_sample(input[0][i])
+                + self.delay_four.process_sample(input[0][i]))
+                / 2.0;
+            output[0][i] = ((self.allpass_three.process_sample(
+                self.allpass_two
+                    .process_sample(self.allpass_one.process_sample(delays_summed)),
+            )) * model.g[i])
+                + input[0][i];
             output[1][i] = input[1][i];
         }
-    }            
+    }
 }
 
 baseplug::vst2!(Reverb, b"rvrb");
